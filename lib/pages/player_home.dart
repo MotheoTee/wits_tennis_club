6da218efe375b7e97cefcf56757b85d7b2a5036d
Motheo Tsirwe <2329751@students.wits.ac.za>
2023-05-05 10:22:21 +0200

    The player's home page

---
+++ lib/pages/player_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Player_Home extends StatefulWidget {
+  const Player_Home({Key? key}) : super(key: key);
+
+  @override
+  State<Player_Home> createState() => _Player_HomeState();
+}
+
+class _Player_HomeState extends State<Player_Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Text('players home in screen'),),
+    );
+  }
+}
