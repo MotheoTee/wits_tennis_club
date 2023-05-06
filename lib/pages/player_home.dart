c96b1151b926abbe43c5bc6a4ebbabbadc828685
Motheo Tsirwe <2329751@students.wits.ac.za>
2023-05-06 12:40:23 +0200

    players home page working with the logout button

--- lib/pages/player_home.dart
+++ lib/pages/player_home.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:wits_tennis_club/pages/sign_in.dart';
 
 class Player_Home extends StatefulWidget {
   const Player_Home({Key? key}) : super(key: key);
@@ -11,7 +13,23 @@ class _Player_HomeState extends State<Player_Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('signed in')),
+      body: Center(
+        child: ElevatedButton(onPressed: () {
+          FirebaseAuth.instance.signOut().then((value) =>
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) =>
+                  Sign_In())).onError((error, stackTrace) =>
+                  print("Error ${error.toString()}")));
+              print("Signed Out Successfully");
+        },child: const Center(
+            child: Text(
+              'LogOut',
+              style: TextStyle(color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15
+              ),
+            )),),
+      ),
     );
   }
 }
