24cc3d40d6b7b2988ee870c7cd9f3c194088b2c3
Motheo Tsirwe <2329751@students.wits.ac.za>
2023-05-08 14:58:21 +0200

    fixing the homepage bugs

--- lib/pages/player_home.dart
+++ lib/pages/player_home.dart
@@ -104,16 +104,16 @@ class _Player_HomeState extends State<Player_Home> {
             currentPage == DrawerSections.homepage? true : false),
           menuItem(2, "Announcements", Icons.announcement,
               currentPage == DrawerSections.announcements? true : false),
-          menuItem(3, "On Campus", Icons.accessibility_outlined,
+          menuItem(3, "On Campus", Icons.group,
               currentPage == DrawerSections.oncampus? true : false),
           menuItem(4, "Off Campus", Icons.group_off_outlined,
               currentPage == DrawerSections.offcampus? true : false),
           menuItem(5, "Make A Query", Icons.question_mark,
               currentPage == DrawerSections.makequery? true : false),
           menuItem(6, "Settings", Icons.settings,
-              currentPage == DrawerSections.homepage? false : true),
+              currentPage == DrawerSections.settings? true : false),
 
-          const Divider(color: Colors.grey,),
+          const Divider(color: Colors.blue,),
 
           menuItem(7, "Contact Us", Icons.people_alt_outlined,
               currentPage == DrawerSections.contacts? true : false),
@@ -127,7 +127,7 @@ class _Player_HomeState extends State<Player_Home> {
   }
   Widget menuItem(int id, String title, IconData icon, bool selected){
     return Material(
-      color: selected ? Colors.grey[300] : Colors.transparent,
+      color: selected ? Colors.blue[100] : Colors.transparent,
       child: InkWell(
         onTap: (){
           Navigator.pop(context);
